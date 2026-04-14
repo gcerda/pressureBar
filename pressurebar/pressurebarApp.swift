3359c8ce60adb8c937e90ef1025546038365f083
Gabriel Cerdá <gabriel.ceral@gmail.com>
2026-04-14 09:20:57 +0200

    Initial Commit

---
+++ pressurebar/pressurebarApp.swift
@@ -0,0 +1,32 @@
+//
+//  pressurebarApp.swift
+//  pressurebar
+//
+//  Created by gabriel.cerda on 14/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct pressurebarApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ pressurebarTests/pressurebarTests.swift
@@ -0,0 +1,19 @@
+//
+//  pressurebarTests.swift
+//  pressurebarTests
+//
+//  Created by gabriel.cerda on 14/4/26.
+//
+
+import Testing
+@testable import pressurebar
+
+struct pressurebarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
